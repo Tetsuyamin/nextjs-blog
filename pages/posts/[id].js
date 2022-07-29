4d93a9fdd09095c449c95611694a58821ab7659a
Tetsuyamin <rh_98@outlook.com>
2022-07-30 08:32:00 +0900

    new look!

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,6 @@
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import Date from '../../components/date';
-import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -29,12 +28,15 @@ export default function Post({ postData }) {
                 <title>{postData.title}</title>
             </Head>
         <article>
-          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-          <div className={utilStyles.lightText}>
+          <div class='rounded-md border-4 border-b-8 border-amber-200 bg-amber-100'>
+          <div class='m-2 bg-teal-200 rounded border-2 border-teal-600'>
+            <h1 className='text-teal-900 text-5xl font-bold p-2'>{postData.title}</h1>
+            <div className='pl-3 text-teal-50 bg-teal-600 font-medium'>
             <Date dateString={postData.date} />
+            </div>
           </div>
-          <br/>
-          <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
+          <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} className='m-2 p-3 text-slate-100 bg-slate-800 mt-2 border-2 rounded border-slate-400 font-medium'/>       
+          </div>
         </article>
       </Layout>
     );
