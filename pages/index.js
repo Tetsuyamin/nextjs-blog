a6f52cb0f6c6338b6bba8f486694e39ba95c91d0
Tetsuyamin <rh_98@outlook.com>
2022-07-30 09:21:53 +0900

    motion

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,25 @@ import Link from 'next/link';
 import Date from '../components/date';
 import { motion } from "framer-motion"
 
+const list = {
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.1,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    transition: {
+    },
+  },
+}
+
+const item = {
+  visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: 100 },
+}
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -26,23 +45,23 @@ export default function Home({allPostsData}) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className='items-center flex-wrap font-bold bg-cyan-700 text-yellow-200 p-3 text-2xl border-b-4 border-cyan-500'>Blog</h2>
         <div class='p-1'></div>
-        <ul className={utilStyles.list}>
+        <motion.ul className={utilStyles.list} variants={list} initial="hidden" animate="visible">
           {allPostsData.map(({ id, date, title }) => (
             <>
             <Link href={`/posts/${id}`}>
-              <li className='ml-4 mr-2 items-center flex-wrap text-teal-900 font-medium bg-teal-400 hover:bg-teal-300 p-3 cursor-pointer border-solid border-teal-500 border-2 border-b-8 hover:border-b-4 hover:mt-1 rounded-md' key={id}>
+              <motion.li variants={item} className='ml-4 mr-2 items-center flex-wrap text-teal-900 font-medium bg-teal-400 hover:bg-teal-300 p-3 cursor-pointer border-solid border-teal-500 border-2 border-b-8 hover:border-b-4 hover:mt-1 rounded-md' key={id}>
                   <a>{title}</a>
                 <br/>
                 <small className={`text-slate-700`}>
                   <Date dateString={date} />
                 </small>
-              </li>
+              </motion.li>
             </Link>
             <li className='p-1'>
             </li>
             </>
           ))}
-        </ul>
+        </motion.ul>
       </section>
     </Layout>
   );
